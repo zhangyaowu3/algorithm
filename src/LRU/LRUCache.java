76a74fb1a176f55b4173ba0be69efa187f753442
wuzhangyao <1715435126@qq.com>
2020-07-23 22:14:11 +0800

    feat: LRU缓存

---
+++ src/LRU/LRUCache.java
@@ -0,0 +1,31 @@
+package LRU;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1,1);
+        lruCache.put(2,2);
+        lruCache.put(3,3);
+        lruCache.get(2);
+        System.out.println(lruCache);
+    }
+}
