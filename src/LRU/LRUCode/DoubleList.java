3dcfe6b85093af0b219e5050fcc0f32d33ea147e
wuzhangyao <1715435126@qq.com>
2020-07-27 00:11:26 +0800

    feat: 双向链表待完善

---
+++ src/LRU/LRUCode/DoubleList.java
@@ -0,0 +1,25 @@
+package LRU.LRUCode;
+
+public class DoubleList {
+
+    // 在链表头部添加节点 x，时间 O(1)
+    public void addFirst(Node x) {
+
+    }
+
+    // 删除链表中的 x 节点（x 一定存在）
+    // 由于是双链表且给的是目标 Node 节点，时间 O(1)
+    public void remove(Node x) {
+
+    }
+
+    // 删除链表中最后一个节点，并返回该节点，时间 O(1)
+    public Node removeLast() {
+        return null;
+    }
+
+    // 返回链表长度，时间 O(1)
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/LRU/LRUCode/Node.java
@@ -0,0 +1,15 @@
+package LRU.LRUCode;
+
+public class Node {
+
+
+    public int key, val;
+    public Node next, prev;
+
+    public Node(int k, int v) {
+        this.key = k;
+        this.val = v;
+    }
+
+
+}
