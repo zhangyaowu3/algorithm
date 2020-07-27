c613b3825e557adb020309ef1a3076559714978b
wuzhangyao <1715435126@qq.com>
2020-07-28 00:37:14 +0800

    feat: leetcode 1518 换酒问题

---
+++ src/贪心算法/ChangeWine.java
@@ -0,0 +1,22 @@
+package 贪心算法;
+
+/**
+ * leetcode 1518 换酒问题 贪心算法
+ * numBottles 酒的数量
+ * numExchange 几个空瓶可以换一瓶酒
+ */
+public class ChangeWine {
+
+    public static void main(String[] args) {
+        System.out.println(numWaterBottles(15, 4));
+    }
+
+    public static int numWaterBottles(int numBottles, int numExchange) {
+        int num = numBottles;
+        while (numBottles >= numExchange) {
+            num = num + numBottles / numExchange;
+            numBottles = numBottles / numExchange + numBottles % numExchange;
+        }
+        return num;
+    }
+}
