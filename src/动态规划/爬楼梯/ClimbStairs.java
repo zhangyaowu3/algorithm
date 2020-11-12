68913043a8bba205f11d4ff7bd8f0641a37d4119
wuzhangyao <1715435126@qq.com>
2020-11-12 12:18:12 +0800

    feat: 回溯算法 全排列 LeetCode 46

--- src/动态规划/爬楼梯/ClimbStairs.java
+++ src/动态规划/爬楼梯/ClimbStairs.java
@@ -3,6 +3,10 @@ package 动态规划.爬楼梯;
 import java.util.Scanner;
 
 /**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
  * leetcode 70
  */
 public class ClimbStairs {
@@ -13,10 +17,14 @@ public class ClimbStairs {
     }
 
     public static int ClimbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
         int[] dp = new int[n + 1];
-        dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
---
+++ src/递归回溯分治/全排列/Solution.java
@@ -0,0 +1,60 @@
+package 递归回溯分治.全排列;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+ *
+ * 输入: [1,2,3]
+ * 输出:
+ * [
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
+ * ]
+ *
+ * leetcode 46  全排列
+ */
+public class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{1, 2, 3};
+        solution.permute(nums);
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        // 记录「路径」
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, track);
+        System.out.println(res);
+        return res;
+    }
+
+    // 路径： 记录在 track 中
+    // 选择列表： nums 中不存在于 track 的那些元素
+    // 结束条件： nums 中的元素全都在 track 中出现
+    void backtrack(int[] nums, LinkedList<Integer> track) {
+        // 触发结束条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 排除不合法的选择
+            if (track.contains(nums[i]))
+                continue;
+            // 做选择
+            track.add(nums[i]);
+            // 进入下一层决策树
+            backtrack(nums, track);
+            // 取消选择
+            track.removeLast();
+        }
+    }
+}
