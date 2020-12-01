2108b8ce869b504643c8db0f90af25e0af30f614
wuzhangyao <1715435126@qq.com>
2020-12-01 21:26:27 +0800

    feat: 代码优化

--- src/算法/排序/前K个高频元素/Solution.java
+++ src/算法/排序/前K个高频元素/Solution.java
@@ -1,9 +1,6 @@
 package 算法.排序.前K个高频元素;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
@@ -18,39 +15,44 @@ import java.util.PriorityQueue;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = new int[] {1,1,1,2,2,3};
+        int[] nums = new int[] {4,1,-1,2,-1,2,3};
         int k = 2;
         solution.topKFrequent(nums, k);
     }
 
-    // 先把数据依次放入map的数据结构中，key为出现的值，value出现的次数
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> hashMap = new HashMap();
-        for(int num : nums) {
-            if (hashMap.containsKey(num)) {
-                hashMap.put(num, hashMap.get(num) + 1 );
+    public int[] topKFrequent(int[] nums, int k) {
+        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
+        HashMap<Integer,Integer> map = new HashMap();
+        for(int num : nums){
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
             } else {
-                hashMap.put(num, 1);
+                map.put(num, 1);
             }
         }
-
-        // 使用小顶堆，放入出现频次较高的元素
-        PriorityQueue priorityQueue = new PriorityQueue();
-        for(Integer key: hashMap.keySet()) {
-           if (priorityQueue.size() < k) {
-               priorityQueue.add(key);
-           } else if (hashMap.get(key) > hashMap.get(priorityQueue.peek())) {
-               priorityQueue.poll();
-               priorityQueue.add(key);
-           }
+        // 遍历map，用最小堆保存频率最大的k个元素
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                // 这个地方是值的比较，而不是key   需要特别注意
+                return map.get(a) - map.get(b);
+            }
+        });
+        for (Integer key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.add(key);
+            } else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove();
+                pq.add(key);
+            }
         }
-
-        // 取出最小堆的元素
-        List<Integer> res = new ArrayList<>();
-        while (!priorityQueue.isEmpty()) {
-            res.add((Integer) priorityQueue.remove());
+        // 取出最小堆中的元素
+        int[] res = new int[k];
+        int idx = 0;
+        for (int num: pq) {
+            res[idx++] = num;
         }
-        System.out.println(String.valueOf(res));
+        System.out.println(res);
         return res;
     }
 }
